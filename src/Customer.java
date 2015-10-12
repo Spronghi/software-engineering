683b9c3c6fd0a83c8a68469ef80508a8c0d170a2
spronghi <sim.colaci@gmail.com>
2015-10-12 17:37:00 +0200

    replacing spaces instead of tabs

--- src/Customer.java
+++ src/Customer.java
@@ -9,48 +9,48 @@ public class Customer {
 
     public Customer(String name, String surname, String email, 
 	                String username, String password, String telephone){
-		this.name=name;
-		this.surname=surname;
-		this.email=email;
-		this.username=username;
-		this.password=password;
-		this.telephone=telephone;
+        this.name=name;
+        this.surname=surname;
+        this.email=email;
+        this.username=username;
+        this.password=password;
+        this.telephone=telephone;
     }
     
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSurname() {
-		return surname;
-	}
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getTelephone() {
-		return telephone;
-	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
