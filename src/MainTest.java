a538e3b2a29fad057d54319e8a0c43d64c3bf7dc
spronghi <sim.colaci@gmail.com>
2015-10-13 12:44:32 +0200

    main class added

---
+++ src/MainTest.java
@@ -0,0 +1,11 @@
+import database.TableData;
+
+
+public class MainTest {
+
+    public static void main(String[] args){
+        TableData istance = new TableData();
+        System.out.println(istance.getAvaibleCar());
+    }
+
+}
