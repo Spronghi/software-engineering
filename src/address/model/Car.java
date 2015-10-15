a23ee306474f9382622fbf471990cc7ea38100c3
spronghi <sim.colaci@gmail.com>
2015-10-15 12:05:06 +0200

    Changed Car.lastKm from String to Integer

--- src/address/model/Car.java
+++ src/address/model/Car.java
@@ -1,24 +1,33 @@
 package address.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Car {
+    private final StringProperty name;
     private final StringProperty licensePlate;
-    private final StringProperty lastKm;
+    private final IntegerProperty lastKm;
     private final StringProperty category;
     private final StringProperty status;
-    
-    public Car(String licensePlate, String lastKm, String category, String status) {
+    public Car(){
+    	this(null, null, 0, null, null);
+    }
+    public Car(String name, String licensePlate, int lastKm, String category, String status) {
+        this.name = new SimpleStringProperty(name);
         this.licensePlate = new SimpleStringProperty(licensePlate);
-        this.lastKm = new SimpleStringProperty(lastKm);
+        this.lastKm = new SimpleIntegerProperty(lastKm);
         this.category = new SimpleStringProperty(category);
         this.status = new SimpleStringProperty(status);
+    }
+    public String getName(){
+        return name.get();
     }
     public String getLicensePlate() {
         return licensePlate.get();
     }
-    public String getLastKm() {
+    public Integer getLastKm() {
         return lastKm.get();
     }
     public String getCategory(){
@@ -27,11 +36,14 @@ public class Car {
     public String getStatus(){
         return status.get();
     }
+    public void setName(String name){
+        this.name.set(name);
+    }
     public void setLicensePlate(String licensePlate) {
         this.licensePlate.set(licensePlate);
     }
-    public void setLastKm(String lastKm) {
-        this.licensePlate.set(lastKm);;
+    public void setLastKm(int lastKm) {
+        this.lastKm.set(lastKm);
     }
     public void setCategory(String category){
         this.category.set(category);
@@ -39,10 +51,13 @@ public class Car {
     public void setStatus(String status){
         this.status.set(status);
     }
+    public StringProperty nameProperty() {
+        return name;
+    }
     public StringProperty licensePlateProperty() {
         return licensePlate;
     }
-    public StringProperty lastKmProperty() {
+    public IntegerProperty lastKmProperty() {
         return lastKm;
     }
     public StringProperty categoryProperty(){
