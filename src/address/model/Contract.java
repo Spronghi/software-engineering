98704d620f4aecd15b39d55c353e9cbf8b919085
spronghi <sim.colaci@gmail.com>
2015-10-17 17:49:42 +0200

    Contract dao class

---
+++ src/address/model/Contract.java
@@ -0,0 +1,9 @@
+package address.model;
+
+import javafx.beans.property.IntegerProperty;
+
+public class Contract {
+	private int id;
+	private IntegerProperty contractNo;
+	private DateProperty date;
+}
