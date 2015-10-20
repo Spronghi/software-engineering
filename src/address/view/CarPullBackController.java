1ba6955bf44cc75045a9b0826a1ff405480d7612
spronghi <sim.colaci@gmail.com>
2015-10-20 20:48:18 +0200

    now you can pull back your car i didn't consider money

--- src/address/view/CarPullBackController.java
+++ src/address/view/CarPullBackController.java
@@ -1,6 +1,8 @@
 package address.view;
 
+import database.Data;
 import address.model.Car;
+import address.model.Contract;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -16,18 +18,64 @@ public class CarPullBackController extends CarEditDialogController{
 	@FXML
 	private Label ammountLabel;
 	@FXML
+	private Label agencyNameLabel;
+	@FXML
+	private Label agencyCityLabel;
+	@FXML
+	private Label agencyCapLabel;
+	@FXML
+	private Label agencyAddressLabel;
+	@FXML
+	private Label startDateLabel;
+	@FXML
+	private Label limitDateLabel;
+	@FXML
+	private Label contractTypeLabel;
+	@FXML
+	private Label firstNameLabel;
+	@FXML
+	private Label lastNameLabel;
+	@FXML
+	private Label telephoneLabel;
+	@FXML
+	private Label emailLabel;
+	@FXML
+	private Label carNameLabel;
+	@FXML
+	private Label licensePlateLabel;
+	@FXML
+	private Label statusLabel;
+	
+	@FXML
 	private TextField kmField;
 	
  	private Stage dialogStage;
+ 	private Contract contract;
  	private Car car;
     private boolean payClicked = false;
 
     @FXML
     private void initialize() {
+
     }
     
 	public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
+       	contract = Data.contract().get(car);
+    	firstNameLabel.setText(contract.getCustomer().getFirstName());
+    	lastNameLabel.setText(contract.getCustomer().getLastName());
+    	telephoneLabel.setText(contract.getCustomer().getTelephone());
+    	emailLabel.setText(contract.getCustomer().getEmail());
+    	carNameLabel.setText(contract.getCar().getName());
+    	licensePlateLabel.setText(contract.getCar().getLicensePlate());
+    	statusLabel.setText(contract.getCar().getStatus());
+    	agencyNameLabel.setText(contract.getAgency().getName());
+    	agencyCityLabel.setText(contract.getAgency().getLocation().getCity());
+    	agencyCapLabel.setText(contract.getAgency().getLocation().getCap());
+    	agencyAddressLabel.setText(contract.getAgency().getLocation().getAddress1());
+    	startDateLabel.setText(contract.getStartDate());
+    	limitDateLabel.setText(contract.getReturnLimitDate());
+    	contractTypeLabel.setText(contract.getContractType());
     }
 	public void setCar(Car car){
 		this.car = car;
@@ -76,7 +124,6 @@ public class CarPullBackController extends CarEditDialogController{
 				result = result + currentKm;
 				travLabel.setText(Integer.toString(result));
 			}
-			
 		}
 	}
 	
