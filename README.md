ec48a957747fb80c72994f60ddc26c6d14855326
spronghi <sim.colaci@gmail.com>
2015-11-03 16:31:35 +0100

    A couple of days without commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Software Engineering
-The repository contains the software engineering project
--- src/database/AgencyDataInterface.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-import javafx.collections.ObservableList;
-import address.model.Agency;
-
-public interface AgencyDataInterface {
-	public ObservableList<Agency> getContainer();
-	public Agency get(String name);
-}
