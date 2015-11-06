ee0d2f746c5799cec540a2513b503248b47cd6d2
spronghi <sim.colaci@gmail.com>
2015-11-06 10:02:31 +0100

    Query to initialize the database tables

---
+++ src/interation/database/Queries.java
@@ -0,0 +1,12 @@
+package interation.database;
+
+public final class Queries {
+	private static String[] queries = {Constants.CREATE_DB,Constants.USE_DB,Constants.CREATE_OPERATOR,
+		Constants.CREATE_CUSTOMER,Constants.CREATE_LOCATION,Constants.CREATE_AGENCY,
+		Constants.CREATE_CAR_STATUS,Constants.CREATE_CAR_CATEGORY,Constants.CREATE_CAR,
+		Constants.CREATE_CONTRACT_TYPE,Constants.CREATE_PAYMENT_TYPE,Constants.CREATE_CURRENCY,
+		Constants.CREATE_PAYMENT,Constants.CREATE_CONTRACT};
+	static String[] array(){
+		return queries;
+	}
+}
