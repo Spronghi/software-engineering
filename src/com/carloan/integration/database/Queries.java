9af7c71a31b19281360ea17f436f7eda72499ba3
spronghi <sim.colaci@gmail.com>
2015-12-02 14:19:51 +0100

    Constant and Queries classes I'm not sure about these classes, they contains the query to initialize db and tables.

---
+++ src/com/carloan/integration/database/Queries.java
@@ -0,0 +1,12 @@
+package com.carloan.integration.database;
+
+public final class Queries {
+	private static String[] queries = {Constants.CREATE_DB,Constants.CREATE_OPERATOR,
+		Constants.CREATE_CUSTOMER,Constants.CREATE_LOCATION,Constants.CREATE_AGENCY,
+		Constants.CREATE_CAR_STATUS,Constants.CREATE_CAR_CATEGORY,Constants.CREATE_CAR,
+		Constants.CREATE_CONTRACT_TYPE,Constants.CREATE_PAYMENT_TYPE,Constants.CREATE_CURRENCY,
+		Constants.CREATE_CONTRACT,Constants.CREATE_PAYMENT};
+	static String[] array(){
+		return queries;
+	}
+}
